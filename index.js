9a6fd47696b5250973e28c05597f7f79b589c1ab
marlonanthony <mad1083@gmail.com>
2019-06-22 00:54:34 -0400

    refactored register and login. Added isemail

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { ApolloServer } = require('apollo-server-express')
 const express = require('express') 
 const { typeDefs } = require('./typeDefs') 
-const { resolvers } = require('./resolvers') 
+const resolvers = require('./resolvers') 
 const mongoose = require('mongoose') 
 const session = require('express-session')
 
@@ -31,4 +31,4 @@ mongoose
 .connect(`mongodb+srv://marlon:marlon123456@cluster0-nqgpm.mongodb.net/graphqlauth?retryWrites=true&w=majority`)
 .then(() => app.listen({ port: 4000 }, () => {
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-})).catch(err => console.log(err)) 
+})).catch(err => console.log(err))
--- resolvers.js
+++ resolvers.js
@@ -1,7 +1,8 @@
 const bcrypt = require('bcryptjs')
 const User = require('./models/User')
+const isEmail = require('isemail')
 
-module.exports.resolvers = {
+const resolvers = {
     Query: {
         me: async (_, __, { req }) => {
             if(!req.session.userId) return null 
@@ -12,22 +13,38 @@ module.exports.resolvers = {
 
     Mutation: {
         register: async (_, { email, password, name }) => {
-            const hashedPassword = await bcrypt.hash(password, 12)
-            const user = await new User({
-                email,
-                name,
-                password: hashedPassword
-            })
-            const res = await user.save()
-            return true 
+            try {
+                if (!isEmail.validate(email)) { throw new Error('Invalide Email') }
+                const existingUser = await User.findOne({ email })
+                if(existingUser) { throw new Error('User already exists') }
+                const hashedPassword = await bcrypt.hash(password, 12)
+                const user = await new User({
+                    name,
+                    email,
+                    password: hashedPassword
+                })
+                await user.save()
+                return true 
+            } catch (error) {
+                console.log(error) 
+                throw error 
+            }
         },
         login: async (_, { email, password }, { req }) => {
-            const user = await User.findOne({ email }) 
-            if(!user) { throw new Error('Email or password is incorrect!') }
-            const isEqual = await bcrypt.compare(password, user.password)
-            if(!isEqual) { throw new Error('Email or password is incorrect!') }
-            req.session.userId = user.id 
-            return user 
+            try {
+                if (!isEmail.validate(email)) { throw new Error('Invalide Email') }
+                const user = await User.findOne({ email }) 
+                if(!user) { throw new Error('Email or password is incorrect!') }
+                const isEqual = await bcrypt.compare(password, user.password)
+                if(!isEqual) { throw new Error('Email or password is incorrect!') }
+                req.session.userId = user.id 
+                return user 
+            } catch (error) { 
+                console.log(error) 
+                throw error 
+            }
         }
     }
 }
+
+module.exports = resolvers
