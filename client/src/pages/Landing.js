7d120d5378b4a95913a17f1e9c25dedc65cd4937
marlonanthony <mad1083@gmail.com>
2019-06-25 04:24:24 -0400

    added open short position

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -4,12 +4,14 @@ import { Query, Mutation } from 'react-apollo'
 
 import { CURRENCY_PAIR_INFO } from '../graphql/queries/currencyPairInfo'
 import { OPENPOSITION } from '../graphql/mutations/openPosition'
+import { CLOSEPOSITION } from '../graphql/mutations/closePosition'
 import { meQuery } from '../graphql/queries/me'
 
 const Landing = props => {
     const [currency, setCurrency] = useState('EUR'),
           [toCurrency, setToCurrency] = useState('USD'),
-          [openedAt, setOpenedAt] = useState(0)
+          [openedAt, setOpenedAt] = useState(0),
+          [closedAt, setClosedAt] = useState(0)
     let me 
 
     return (
@@ -21,52 +23,73 @@ const Landing = props => {
                     const user = client.readQuery({ query: meQuery })
                     if(user && user.me && user.me.id) me = user.me
                 return (
-                    <main style={{marginTop: 100}}>
-                        <select 
-                            name='currency'
-                            value={currency}
-                            onChange={e => setCurrency(e.target.value)}
-                        >
-                            <option>EUR</option>
-                            <option>USD</option>
-                            <option>GBP</option>
-                            <option>NZD</option>
-                            <option>AUD</option>
-                        </select>
-                        <select 
-                            name='toCurrency'
-                            value={toCurrency}
-                            onChange={e => setToCurrency(e.target.value)}
-                        >
-                            <option>JPY</option>
-                            <option>CHF</option>
-                            <option>CAD</option>
-                            <option>USD</option>
-                        </select>
-                        <button onClick={() => refetch()}>Refresh</button>
-                        { setOpenedAt(+data.currencyPairInfo.askPrice) }
-                        { me && (
-                            <Mutation
-                                mutation={OPENPOSITION}
-                                variables={{ pair: `${currency}/${toCurrency}`, lotSize: 100000, openedAt, position: 'long' }}
-                                >
-                                {(openPosition, { data, loading, error }) => {
-                                if(loading) return <p>Loading</p>
-                                if(error) {
-                                    console.log(error)  
-                                    return <small style={{color: 'white'}}>Error: { error.message }</small>
-                                }
-                                return (openPosition && 
-                                    <>
-                                        <button onClick={openPosition}>Buy</button>
-                                        <div>
+                    <main style={{marginTop: 100 }}>
+                        <div>
+                            <select 
+                                name='currency'
+                                value={currency}
+                                onChange={e => setCurrency(e.target.value)}
+                            >
+                                <option>EUR</option>
+                                <option>USD</option>
+                                <option>GBP</option>
+                                <option>NZD</option>
+                                <option>AUD</option>
+                            </select>
+                            <select 
+                                name='toCurrency'
+                                value={toCurrency}
+                                onChange={e => setToCurrency(e.target.value)}
+                            >
+                                <option>JPY</option>
+                                <option>CHF</option>
+                                <option>CAD</option>
+                                <option>USD</option>
+                            </select>
+                            <button onClick={() => refetch()}>Refresh</button>
+                            { setOpenedAt(+data.currencyPairInfo.askPrice) }
+                            { setClosedAt(+data.currencyPairInfo.bidPrice) }
+                            { me && (
+                                <Mutation
+                                    mutation={OPENPOSITION}
+                                    variables={{ pair: `${currency}/${toCurrency}`, lotSize: 100000, openedAt, position: 'long' }}
+                                    >
+                                    {(openPosition, { data, loading, error }) => {
+                                    if(loading) return <p>Loading</p>
+                                    if(error) {
+                                        console.log(error)  
+                                        return <small>Error: { error.message }</small>
+                                    }
+                                    return ( openPosition && 
+                                        <>
+                                            <button onClick={openPosition}>Buy</button>
                                             <p>{data && data.openPosition.message}</p>
-                                        </div>
-                                    </>
-                                )
-                                }}
-                            </Mutation>
-                        )}
+                                        </>
+                                    )
+                                    }}
+                                </Mutation>
+                            )}
+                            { me && (
+                                <Mutation
+                                    mutation={OPENPOSITION}
+                                    variables={{ pair: `${currency}/${toCurrency}`, lotSize: 100000, openedAt, position: 'short' }}
+                                    >
+                                    {(openPosition, { data, loading, error }) => {
+                                    if(loading) return <p>Loading</p>
+                                    if(error) {
+                                        console.log(error)  
+                                        return <small>Error: { error.message }</small>
+                                    }
+                                    return ( openPosition && 
+                                        <>
+                                            <button onClick={openPosition}>Sell</button>
+                                            <p>{data && data.openPosition.message}</p>
+                                        </>
+                                    )
+                                    }}
+                                </Mutation>
+                            )}
+                        </div>
                         {
                             data && data.currencyPairInfo && Object.keys(data.currencyPairInfo).map(val =>(
                                 <main key={Math.random()}>
