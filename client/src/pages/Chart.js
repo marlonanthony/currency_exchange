8a8f9e3f44e3ed2c8dc0e891777164d40b427261
marlonanthony <mad1083@gmail.com>
2019-06-27 22:51:40 -0400

    created form to update charts currency pair

--- client/src/pages/Chart.js
+++ client/src/pages/Chart.js
@@ -1,51 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Line } from 'react-chartjs-2'
 import { Query } from 'react-apollo'
 
 import { MONTHLYTIMESERIES } from '../graphql/queries/monthlyTimeSeries'
 
-const Chart = () => (
-    <Query query={MONTHLYTIMESERIES} variables={{ fc: 'EUR', tc: 'USD' }}>
-    {({ data, loading, error }) => {
-        if(loading) return <h1>Loading...</h1>
-        if(error) return `Error ${error}`
-        if(data) {
-            const labels = data.monthlyTimeSeries.timesArray
-            const chartData = data.monthlyTimeSeries.valuesArray
-            return (
-                <div>
-                    <h3>Chart</h3>
-                    <Line data={{
-                        labels,
-                        datasets: [
-                            {
-                                label: `${'EUR'}/${'USD'} Time Series FX (Monthly)`,
-                                fill: true,
-                                lineTension: 0.1,
-                                backgroundColor: 'rgba(75,192,192,0.4)',
-                                borderColor: 'rgba(75,192,192,1)',
-                                borderCapStyle: 'butt',
-                                borderDash: [],
-                                borderDashOffset: 0.0,
-                                borderJoinStyle: 'miter',
-                                pointBorderColor: 'rgba(75,192,192,1)',
-                                pointBackgroundColor: '#fff',
-                                pointBorderWidth: 1,
-                                pointHoverRadius: 5,
-                                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                                pointHoverBorderColor: 'rgba(220,220,220,1)',
-                                pointHoverBorderWidth: 2,
-                                pointRadius: 1,
-                                pointHitRadius: 10,
-                                data: chartData
-                            }
-                        ]
-                    }} />
-                </div>
-            )
-        }
-    }}
-    </Query>
+const Chart = () => {
+    const [fc, setFc] = useState('EUR'),
+          [tc, setTc] = useState('USD'), 
+          [fromCurrency, setFromCurrency] = useState('EUR'), 
+          [toCurrency, setToCurrency] = useState('USD')
+
+    return (
+        <Query query={MONTHLYTIMESERIES} variables={{ fc: fromCurrency, tc: toCurrency }}>
+        {({ data, loading, error }) => {
+            if(loading) return <h1>Loading...</h1>
+            if(error) return `Error ${error}`
+            if(data) {
+                const labels = data.monthlyTimeSeries.timesArray
+                const chartData = data.monthlyTimeSeries.valuesArray
+                return (
+                    <div>
+                        <h3>Chart</h3>
+                        <form onSubmit={e => {
+                            e.preventDefault()
+                            setFromCurrency(fc)
+                            setToCurrency(tc) 
+                        }}>
+                            <input 
+                                name='fc'
+                                value={fc}
+                                placeholder='From Currency'
+                                onChange={e => setFc(e.target.value)}
+                            />
+                            <input 
+                                name='tc'
+                                value={tc}
+                                placeholder='From Currency'
+                                onChange={e => setTc(e.target.value)}
+                            />
+                            <button>submit</button>
+                        </form>
+                        <Line data={{
+                            labels,
+                            datasets: [
+                                {
+                                    label: `${fromCurrency}/${toCurrency} Time Series FX (Monthly)`,
+                                    fill: true,
+                                    lineTension: 0.1,
+                                    backgroundColor: 'rgba(75,192,192,0.4)',
+                                    borderColor: 'rgba(75,192,192,1)',
+                                    borderCapStyle: 'butt',
+                                    borderDash: [],
+                                    borderDashOffset: 0.0,
+                                    borderJoinStyle: 'miter',
+                                    pointBorderColor: 'rgba(75,192,192,1)',
+                                    pointBackgroundColor: '#fff',
+                                    pointBorderWidth: 1,
+                                    pointHoverRadius: 5,
+                                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                                    pointHoverBorderColor: 'rgba(220,220,220,1)',
+                                    pointHoverBorderWidth: 2,
+                                    pointRadius: 1,
+                                    pointHitRadius: 10,
+                                    data: chartData
+                                }
+                            ]
+                        }} />
+                    </div>
+                )
+            }
+        }}
+        </Query>
     )
+}
 
 export default Chart
